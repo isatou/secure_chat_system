a24b4d85ee1d7905ded2323becec5be0250583cb
Isatou <isatou@isatou-Inspiron-1545.(none)>
2012-12-03 23:12:40 -0500

    removed commented unused code

--- register.py
+++ register.py
@@ -1,10 +1,3 @@
-
-
-'''
-session key = random variable 
-hash of password: private key used by client for decryption
-'''
-
 import sys
 from subprocess import *
 import cPickle as pcl
@@ -13,8 +6,6 @@ from Crypto.Random import random as R
 import hashlib as H
 import string
 import pickle
-
-
 
 
 # open file
@@ -38,8 +29,6 @@ password=raw_input('Choose a password: ')
 for i in range(0,100):
 	password_hash=H.sha512(password).hexdigest()
 	password=password_hash
-
-
 
 users[str(username)]=password
 
@@ -75,45 +64,6 @@ pickle.dump(state,file)
 
 # close the file, and your pickling is complete
 file.close()
-
-
-
-
-'''users={'bob': 'pass1', 'alice': 'pass2', 'cindy': 'pass3', 'john': 'pass4'}
-
-users={'bob': 'c2457d3096fc9c2b6f6bf0ffc936f47812073a34412f9b6f8162afacfe6ea5ff5aab46fcc12060c6ac1cae57f83b4905fbe14a4a32bad62c820720a51159345f', 'alice': '5d8562f8212ed3712585f93603c2ad09ed258ce8d4d63c351e99ee51359e4f17522635f5e28b1410abc26fdef2e91ce3c6307a9962b515154997b7a82c0ce3cb', 'cindy': '7b35381b6bde7e3c3536d59566a217ea4af76786ad94220231b20a522532b8b4384a4b5a9c4e3291cd2d6af11f030fa365e6f7e1feedc4c3fa17a7785c4f5b83', 'john': 'f91b850a442e203da03049c509cb1d5c7b18dcab7f581318efc838f6720b85d617608f31ce2f7d12e6d03e2f804ca329f3f7376a122f09542ef4af5482dcc8af'}
-
-
-
-password="pass4"
-#hash password 100 times 
-#hash of password is used as secret key for client
-commitment=H.sha256(password).hexdigest()
-print H.sha512(password).digest_size
-#print commitment
-for i in range(0,100):
-	commitment=H.sha1(password).hexdigest()
-	password=commitment
-
-print "commitment"
-print commitment
-
-if(('alice' in users) == 1):
-	print "yes"
-print users
-
-
-
-from Crypto import Random
-from Crypto.Random import random as R
-import string
-
-
-#randomly make a guess
-random_number = R.StrongRandom()'''
-
-
-
 
 
 
